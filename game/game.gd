8c11d15f9e3860ec53cb5f8c057b5d32d3a867b4
Rein van der Woerd <reinvanderwoerd@me.com>
2025-06-24 09:58:35 +0200

    combined everything in one scene

--- game/game.gd
+++ game/game.gd
@@ -2,7 +2,62 @@ extends Node3D
 
 @export var module_vein_straight: PackedScene 
 
+# Global 
+enum GAME_STATE {
+	OUTFIT_SELECT,
+	PLAYING
+}
+
+@export var game_state = GAME_STATE.OUTFIT_SELECT
+
+
+# Movement
+@export var forward_speed = 30
+@export var forward_speed_menu = 3
+
+var move_speed = 10
+var velocity = Vector2(0,0)
+var drag = 0.9
+
+
+
 func _process(delta):
+	# Playing
+	if game_state == GAME_STATE.PLAYING:
+		handle_ship_movement(delta)
+		$LevelPosition.translate(Vector3(0,0, -forward_speed * delta))
+	
+	if game_state == GAME_STATE.OUTFIT_SELECT:
+		$LevelPosition.translate(Vector3(0,0, -forward_speed_menu * delta))
+	
+	lerp_camera()
+	update_track()
+	
+	
+	
+	
+func handle_ship_movement(delta):
+	var x = Input.get_axis("ui_left", "ui_right")
+	var y = Input.get_axis("ui_down", "ui_up")
+	
+	velocity.x += x * move_speed * delta
+	velocity.y += y * move_speed * delta
+	
+	velocity *= drag
+	
+	$Ship.translate(Vector3(velocity.x, velocity.y, 0))
+
+
+
+func lerp_camera():
+	# Always move camera
+	$LevelPosition/Camera3D.position.x = lerp($LevelPosition/Ship.position.x, $LevelPosition/Camera3D.position.x, 0.95)
+	$LevelPosition/Camera3D.position.y = lerp($LevelPosition/Ship.position.y, $LevelPosition/Camera3D.position.y, 0.95)
+	
+
+
+func update_track():
+	# Track logic
 	var first_track_piece: TrackModule = $Track.get_children().get(0)
 	var second_track_piece: TrackModule = $Track.get_children().get(1)
 	var last_track_piece: TrackModule = $Track.get_children().get(3)
@@ -20,5 +75,3 @@ func _process(delta):
 		new_module.position = last_track_piece.position - Vector3(0,0,70)
 		#new_module.rotate(Vector3(0,1,0), 90)
 		$Track.add_child(new_module)
-		
-	
